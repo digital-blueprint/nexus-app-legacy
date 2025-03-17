62a19ce2b78c2310b118e74d4f1f5c713ec34790
Christoph Reiter <reiter.christoph@gmail.com>
2025-03-17 15:05:11 +0100

    Add a dummy test back
    otherwise the test command fails with 0 tests

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,9 @@
+import { assert } from 'chai';
+
 import '../src/dbp-nexus-app.js';
 
 suite('dbp-template-activity basics', () => {
-
+    test('dummy', () => {
+        assert(true);
+    });
 });
